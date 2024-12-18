00951ce61478882cd2af551389bffd59961e29c8
Muriloferreiraa <155457001+Muriloferreiraa@users.noreply.github.com>
2024-12-18 10:46:42 -0300

    Co-authored-by: Rodrigo Junior <rodrigojr09@users.noreply.github.com>

--- src/pages/aso/riscos/cadastro.js
+++ src/pages/aso/riscos/cadastro.js
@@ -1,57 +1,56 @@
 import React, { useState } from "react";
-function Cadastro (){
-  function handleSave() {}
+function Cadastro() {
+  function handleSave() { }
 
   return <div className="bg-gradient-to-r from-black via-pink-500 to-black min-h-screen flex">
-      <div className="w-64 min-h-screen fixed bg-black/80 text-white flex flex-col p-6 shadow-lg">
-        <h2 className="mb-4 text-xl font-bold">Menu</h2>
-         
-        <ul className="space-y-4">
-          <li>
-            <button
-              className="w-full text-left font-semibold"
-              id="info-basicas-btn"
-              onClick={() => navigateTo("/")}
-            >
-              1. Informações Básicas
-            </button>
-          </li>
-          <li>
-            <button
-              className="w-full text-left font-semibold"
-              id="riscos-btn"
-              disabled
-            >
-              1.1. Riscos
-            </button>
-          </li>
-          <li>
-            <button
-              className="w-full text-left ml-8 font-semibold text-gray-500"
-              id="exames-btn"
-              disabled
-            >
-              1.1.1. Exames
-            </button>
-          </li>
-          <li>
-            <button
-              className="w-full text-left font-semibold text-gray-500"
-              id="resumo-btn"
-              disabled
-            >
-              2. Resumo
-            </button>
-          </li>
-        </ul>
-      </div>
-
+    <div className="w-64 min-h-screen fixed bg-black/80 text-white flex flex-col p-6 shadow-lg">
+      <h2 className="mb-4 text-xl font-bold">Menu</h2>
+      <ul className="space-y-4">
+        <li>
+          <button
+            className="w-full text-left font-semibold"
+            id="info-basicas-btn"
+            onClick={() => navigateTo("/")}
+          >
+            1. Informações Básicas
+          </button>
+        </li>
+        <li>
+          <button
+            className="w-full text-left font-semibold"
+            id="riscos-btn"
+            disabled
+          >
+            1.1. Riscos
+          </button>
+        </li>
+        <li>
+          <button
+            className="w-full text-left ml-8 font-semibold text-gray-500"
+            id="exames-btn"
+            disabled
+          >
+            1.1.1. Exames
+          </button>
+        </li>
+        <li>
+          <button
+            className="w-full text-left font-semibold text-gray-500"
+            id="resumo-btn"
+            disabled
+          >
+            2. Resumo
+          </button>
+        </li>
+      </ul>
       <div className="w-full h-full flex ml-64 items-center justify-center">
         <div className="w-full max-w-xl my-10 rounded-lg bg-black/70 p-8 shadow-lg">
           <h1 className="mb-8 text-center text-3xl font-bold text-white">
             Seleção de Riscos
           </h1>
-          div
+          
+    
+           
           <div className="text-center">
             <button
               className="rounded-lg bg-pink-600 px-6 py-3 font-bold text-white hover:scale-110 focus:outline-none"
@@ -63,7 +62,7 @@ function Cadastro (){
         </div>
       </div>
     </div>
-  
+  </div>
 };
 
-export default Cadastro;
+    export default Cadastro;
